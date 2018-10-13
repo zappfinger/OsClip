a4c49516d01896a2816bb112d0622abc73d5d128
Richard van Bemmelen <zappfinger@gmail.com>
2018-10-13 13:58:52 +0200

    Add files via upload

---
+++ osclip.py
@@ -0,0 +1,73 @@
+"""
+
+OsClip: bi-directional clipboard sharing between computers (PC or Mac)
+	based on pyperclip and python-osc
+
+by:			zappfinger (Richard van Bemmelen)
+version:	1.0
+date:		13-10-2018
+
+"""
+
+from pythonosc import dispatcher
+from pythonosc import osc_server, udp_client
+import time
+import threading
+import pyperclip
+
+"""
+	enter the ip address of PC or Mac to share the clipboard with
+	enter this machine's ip address on the other machine as well 
+"""
+##########################
+otherIP = '192.168.1.9'
+##########################
+
+magic = 'm@gic:'
+
+class server():
+	def __init__(self, ip, port):
+		self.dispatcher = dispatcher.Dispatcher()
+		self.dispatcher.map("/clip", self.print_clip_handler, "")
+		self.server = osc_server.ThreadingOSCUDPServer((ip, port), self.dispatcher)
+
+	def print_clip_handler(self, unused_addr, args, cliptext):
+		print("{0}".format(cliptext))
+		if magic in cliptext:
+			cliptext = cliptext[6:]
+			pyperclip.copy(cliptext)
+
+class client():
+	def __init__(self, ip, port):
+		self.oldclip = ''
+		self.client = udp_client.SimpleUDPClient(ip, port)
+
+	def send(self):		# send when clipboard has changed
+		while 1:
+			pasted = pyperclip.paste()
+			if not (magic in pasted) and pasted!= self.oldclip:
+				self.client.send_message("/clip", magic + pasted)
+				self.oldclip = pasted
+				print(self.oldclip)
+			time.sleep(1)
+
+if __name__ == "__main__":
+	def worker1():
+		serv = server('0.0.0.0', 8888)
+		print("Serving on {}".format(serv.server.server_address))
+		serv.server.serve_forever()
+
+	def worker2():
+		clint = client(otherIP, 8888)
+		clint.send()
+
+	thread_list = []
+	thread1 = threading.Thread(target=worker1)
+	thread_list.append(thread1)
+	thread1.start()
+	thread2 = threading.Thread(target=worker2)
+	thread_list.append(thread2)
+	thread2.start()
+
+while 1:
+	pass
